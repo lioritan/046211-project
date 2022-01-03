603da5ecb227755b739c7a7a9d31d737ac175b06
lioritan <lioritan@users.noreply.github.com>
2022-01-03 14:12:51 +0200

    Better hyper-params (based on MAML paper)

--- main.py
+++ main.py
@@ -39,17 +39,17 @@ def get_parser():
     parser.add_argument('--shots', default=5, type=int,
                         help="Number of training examples in the inner loop at meta-test time")
     parser.add_argument('--ways', default=5, type=int, help="Number of candidate labels (classes) at meta-test time")
-    parser.add_argument('--train_size', default=80, type=int,
+    parser.add_argument('--train_size', default=20, type=int,
                         help="Number of training examples in the inner loop at meta-train time")
-    parser.add_argument('--per_task_lr', default=0.5, type=int,
+    parser.add_argument('--per_task_lr', default=0.01, type=int,
                         help="Per task LR for adaptation, should be high")
-    parser.add_argument('--meta_lr', default=0.005, type=int,
+    parser.add_argument('--meta_lr', default=0.001, type=int,
                         help="Meta LR")
-    parser.add_argument('--meta_batch_size', default=16, type=int,
+    parser.add_argument('--meta_batch_size', default=4, type=int,
                         help="Number of task gradients to average for meta-gradient step")
-    parser.add_argument('--adaptation_steps', default=1, type=int,
+    parser.add_argument('--adaptation_steps', default=5, type=int,
                         help="Number of gradient steps to take during adaptation, if more than 1, consider lowering per_task_lr")
-    parser.add_argument('--n_epochs', default=20, type=int,
+    parser.add_argument('--n_epochs', default=50, type=int,
                         help="Meta epochs for training")
     parser.add_argument('--dataset', default="mini-imagenet", choices=["mini-imagenet", "omniglot"],
                         help="Dataset to use.")
